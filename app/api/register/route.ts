7178a7eb5d747a4c2710d4e7232a93a306c9dee9
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-06-04 19:23:14 +0100

    Fix: added comments to the register post route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,26 +1,26 @@
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import schema from "./schema";
 import bcrypt from "bcrypt";
-
-const schema = z.object({
-  email: z.string().email(),
-  password: z.string().min(5),
-});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
+  //check if the request matches the schema
   const validation = schema.safeParse(body);
 
+  //if not return the error and set status to 400
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
+  //check if the user exists in the database
   const user = await prisma.user.findUnique({ where: { email: body.email } });
 
+  //if the user exists, return a message and set the status to 400
   if (user)
     return NextResponse.json({ error: "User already exists" }, { status: 400 });
 
+  //otherwise create the user (using a hashed password)
   const hashedPassword = await bcrypt.hash(body.password, 10);
   const newUser = await prisma.user.create({
     data: { email: body.email, hashedPassword },
